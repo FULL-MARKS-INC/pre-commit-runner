dd1c19da6438461bf2bdf89c142152d31ecafd3d
umezaki <umezaki@fullmarks.co.jp>
2023-08-23 09:13:04 +0900

    shell=True かつ subprocess.runの第一引数を文字列にする

--- pre_commit_runner/main.py
+++ pre_commit_runner/main.py
@@ -13,12 +13,13 @@ def main():
         for path in sys.argv[2:]
         if (x := pathlib.Path(path)) and x.is_relative_to(project_root)
     ]
-    
+
     if files:
         target_pwd = repository_root / sys.argv[1]
         proc = subprocess.run(
-            ["poetry", "run", "pre-commit", "run", "--files", *[str(p) for p in files]],
+            f"poetry run pre-commit run --files {' '.join([str(p) for p in files])}",
             cwd=str(target_pwd),
+            shell=True
         )
         exit(proc.returncode)
     else:
